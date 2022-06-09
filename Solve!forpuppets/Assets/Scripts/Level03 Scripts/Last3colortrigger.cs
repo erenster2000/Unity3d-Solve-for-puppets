bdfb30b282aa22162f75eeb5587fd813a6c85342
Emir Sonalp <etsnlp@gmail.com>
2022-06-09 21:20:07 +0300

    code fix

--- Solve!forpuppets/Assets/Scripts/Level03 Scripts/Last3colortrigger.cs
+++ Solve!forpuppets/Assets/Scripts/Level03 Scripts/Last3colortrigger.cs
@@ -6,21 +6,15 @@ public class Last3colortrigger : MonoBehaviour
 {
     [SerializeField]
     GameObject box, blackBox;
-    void Start()
-    {
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    int lockerCount = 0;
 
     public void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject == box)
+        if(other.gameObject == box && lockerCount == 0)
         {
             Level03_Controller.Lcounter++;
+            lockerCount++;
         }
 
         if(other.gameObject == blackBox)
